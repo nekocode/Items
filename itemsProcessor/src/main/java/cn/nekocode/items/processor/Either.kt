8fba3150ddbe46ccf80503e616b07ee927f00465
nekocode <nekocode.cn@gmail.com>
2018-11-24 11:59:52 +0800

    Add error handling class Either

---
+++ itemsProcessor/src/main/java/cn/nekocode/items/processor/Either.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018. nekocode (nekocode.cn@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.nekocode.items.processor
+
+/**
+ * See https://ashishchaudhary.in/better-error-handling-in-kotlin-with-either-type
+ * @author nekocode (nekocode.cn@gmail.com)
+ */
+sealed class Either<out T> {
+    data class Success<T>(val value: T) : Either<T>()
+    data class Error(val msg: String?, val e: Exception?) : Either<Nothing>()
+}
